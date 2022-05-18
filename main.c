16f011fd42b956ffe8b23eeaf5b9351783849769
PicuBicu <championello@gmail.com>
2022-05-18 23:10:04 +0200

    Clean up code in main

--- main.c
+++ main.c
@@ -1,111 +1,70 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <assert.h>
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 #include "init.h"
-#define LEVEL_SIGNAL SIGRTMIN
-#define LOG_SIGNAL SIGRTMIN + 1
 
-unsigned long long int factoryNum;
-short int originalNum;
-void *factory(void *arg);
-void *logThread(void *arg);
-unsigned long long int countFactory(short int num);
-static char *isPrime(unsigned long long num);
-void f1(FILE *f);
-void f2(FILE *f);
-char *prime;
-int evenOdd;
+#define LOGGING_STATE_SIGNAL_NUM SIGRTMIN
+#define DUMP_FILE_SIGNAL_NUM SIGRTMIN + 1
 
-int main()
-{
+void *first_thread_routine(void *arg);
+void *second_thread_routine(void *arg);
+void second_dumper(FILE *file);
+void first_dumper(FILE *file);
+
+int main() {
     srand(time(NULL));
-    printf("pid = %d, level change signal = %d, dump signal = %d\n", getpid(), LEVEL_SIGNAL, LOG_SIGNAL);
-    assert(create_logger(ENABLED
-                         , LEVEL_SIGNAL
-                         , LOG_SIGNAL
-                         , DFL_FILENAME
-                         , 2) == 0);
-    change_logger_priority(MAX);
-    pthread_t tid, tid2;
-    pthread_create(&tid, NULL, factory, (void *)&logging_state_signal);
-    pthread_create(&tid2, NULL, logThread, (void *)&dump_file_signal);
-    pthread_join(tid, NULL);
-    pthread_join(tid2, NULL);
-    destroy_logger();
-    return 0;
-}
-
-void *factory(void *arg)
-{
-    add_dump_file_function(f1);
-
-    while (1)
-    {
-        originalNum = rand() % 16;
-        factoryNum = countFactory(originalNum);
-        log_message(MIN, "Starting looking for a factory number");
-        char buffer[100];
-        sprintf(buffer, "Found a number: %d and the factory: %llu", originalNum, factoryNum);
-        log_message(MIN, buffer);
-        sleep(1);
+    printf("PID = %d\nENABLE/DISABLE LOGGING TO FILE SIGNAL = %d\nDUMP FILE CREATION SIGNAL = %d\n",
+           getpid(),
+           LOGGING_STATE_SIGNAL_NUM, DUMP_FILE_SIGNAL_NUM);
+    if (create_logger(ENABLED,
+                      LOGGING_STATE_SIGNAL_NUM,
+                      DUMP_FILE_SIGNAL_NUM,
+                      DFL_FILENAME, 2) == 0) {
+        pthread_t first_tid, second_tid;
+        pthread_create(&first_tid, NULL, first_thread_routine, NULL);
+        pthread_create(&second_tid, NULL, second_thread_routine, NULL);
+        pthread_join(first_tid, NULL);
+        pthread_join(second_tid, NULL);
+        destroy_logger();
+        return 0;
     }
-    return NULL;
+    return EXIT_FAILURE;
 }
-void *logThread(void *arg)
-{
-    add_dump_file_function(f2);
-    while (1)
-    {
-        char buffer[100];
-        sprintf(buffer, "Checking whether number %llu is even or odd", factoryNum);
-        log_message(STANDARD, buffer);
-        evenOdd = factoryNum % 2;
-        sprintf(buffer, "Checking whether number %llu is prime or composite", factoryNum);
-        log_message(MAX, buffer);
-        prime = isPrime(factoryNum);
+
+void *first_thread_routine(void *arg) {
+    add_dump_file_function(second_dumper);
+    while (1) {
+        log_message(MIN, "Watek pierwszy, pierwsza linia");
+        log_message(STANDARD, "Watek pierwszy, druga linia");
+        log_message(MAX, "Watek pierwszy, trzecia linia");
         sleep(1);
     }
     return NULL;
 }
 
-unsigned long long int countFactory(short int num)
-{
-
-    if (num == 0)
-    {
-        return 1;
+void *second_thread_routine(void *arg) {
+    add_dump_file_function(first_dumper);
+    while (1) {
+        log_message(MIN, "Watek drugi, pierwsza linia");
+        log_message(STANDARD, "Watek drugi, druga linia");
+        log_message(MAX, "Watek drugi, trzecia linia");
+        if (rand() % 12 == 0) {
+            log_message(MAX, "Watek drugi, Zmiana logowania");
+            change_logger_priority(MAX);
+        } else {
+            change_logger_priority(MIN);
+        }
+        sleep(1);
     }
-    unsigned long long int result = 1;
-    for (short int i = 2; i <= num; i++)
-    {
-        result *= i;
-    }
-    return result;
+    return NULL;
 }
 
-static char *isPrime(unsigned long long num)
-{
-    if (num <= 2)
-        return "PRIME";
-    for (unsigned long long i = 2; i <= num / 2; i++)
-    {
-        if (num % i == 0)
-            return "COMPOSITE";
-    }
-    return "PRIME";
+void second_dumper(FILE *file) {
+    fprintf(file, "Register function 1, Random %d\n", rand());
 }
 
-void f1(FILE *f)
-{
-    fprintf(f, "Drawn number is: %d\n", originalNum);
-    fprintf(f, "Factory is: %llu\n", countFactory(originalNum));
-}
-void f2(FILE *f)
-{
-    factoryNum % 2 == 0 ? fprintf(f, "Factory number is even\n") : fprintf(f, "Factory number is odd\n");
-    char *p = isPrime(factoryNum);
-    fprintf(f, "Factory number is %s \n", p);
+void first_dumper(FILE *file) {
+    fprintf(file, "Register function 2, Random %d\n", rand());
 }
